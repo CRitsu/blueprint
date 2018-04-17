3d8817eb26a512c4d5d4e1a7385d929b4e34ead7
Rick <cyanyao@live.com>
2018-04-18 00:21:23 +0800

    first commit

---
+++ src/Blueprint.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Blueprint extends Component {
+  render() {
+    return (
+      <div className="blueprint">
+        blueprint
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Blueprint from './Blueprint';
+
+ReactDOM.render(<Blueprint />, document.getElementById('root'));
